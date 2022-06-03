98bad76c18f2e1681d934d207bc0e06336fa3c6c
MUSTAFA YILMAZ <meka.mustafayilmaz@outlook.com>
2022-06-03 15:21:20 +0300

    Add project files.

---
+++ TodoListAPI/Data/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoListAPI.Data
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        [Required,MaxLength(100)]
+        public string Title { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
